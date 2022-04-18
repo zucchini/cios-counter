b2417a2b76499bf839569bb7dbf26d08a26c20a3
Austin Adams <git@austinjadams.com>
2022-04-18 01:07:01 -0400

    Support beautifulsoup versions older than my great-grandpa (Oops)

--- dehtmlify.py
+++ dehtmlify.py
@@ -16,7 +16,7 @@ def main(argv):
 
     soup = BeautifulSoup(sys.stdin, 'html.parser')
 
-    for course in soup.select('.MyEvalCenterToBeOpened, .MyEvalCenterOpened'):
+    for course in soup.find_all(class_=['MyEvalCenterToBeOpened', 'MyEvalCenterOpened']):
         title = course.select('.classTitle')[0].text
         if title == title_wanted:
             responded_numbers = course.select('.spanRespondedNumbers')
