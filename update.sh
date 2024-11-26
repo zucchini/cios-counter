57a93f0ef478fd9ea1bc9fe8ae244bc737365a71
Austin Adams <git@austinjadams.com>
2024-11-26 16:58:07 -0500

    Support multiple sections

--- update.sh
+++ update.sh
@@ -3,11 +3,11 @@ set -e
 set -o pipefail
 
 [[ -z $1 ]] && {
-    printf 'usage: %s <semester>\n' "$0" >&2
+    printf 'usage: %s <semester_1> <semester_2> ... <semester_n>\n' "$0" >&2
     exit 2
 }
 
-sem=$1
+sems=( "$@" )
 
 [[ ! -e curl.sh ]] && {
     printf 'error: you need to create curl.sh, read the readme pls\n' >&2
@@ -15,28 +15,34 @@ sem=$1
 }
 
 while true; do
-    {
-        printf -- '             CS 2110 CIOS Progress\n'
-        printf -- '             =====================\n'
-        printf -- '\n'
-        . curl.sh | ./dehtmlify.py "$sem" | figlet -c -w 50
-        printf -- '\n'
-        printf -- '              Do your CIOS here:\n'
-        printf -- '      ===>                           <===\n'
-        printf -- '      ===> https://b.gatech.edu/cios <===\n'
-        printf -- '      ===>                           <===\n'
-        printf -- '\n'
-        printf -- '\n'
-        printf -- '\n'
-        printf -- '\n'
-        printf -- '\n'
-        printf -- '------------------------------------------------------\n'
-        printf -- 'last updated: %s\n' "$(TZ=America/New_York date)"
-        printf -- 'source code: https://github.com/zucchini/cios-counter/\n'
-    } >status.next.txt
+    . curl.sh >page.html
 
-    # Update the file atomically if we can (that is, if we crash, leave it alone)
-    mv status.next.txt status.txt
+    for sem in "${sems[@]}"; do
+        mkdir -p "$sem"
+
+        {
+            printf -- '             CS 2110 CIOS Progress\n'
+            printf -- '             =====================\n'
+            printf -- '\n'
+            ./dehtmlify.py "$sem" <page.html | figlet -c -w 50
+            printf -- '\n'
+            printf -- '              Do your CIOS here:\n'
+            printf -- '      ===>                           <===\n'
+            printf -- '      ===> https://b.gatech.edu/cios <===\n'
+            printf -- '      ===>                           <===\n'
+            printf -- '\n'
+            printf -- '\n'
+            printf -- '\n'
+            printf -- '\n'
+            printf -- '\n'
+            printf -- '------------------------------------------------------\n'
+            printf -- 'last updated: %s\n' "$(TZ=America/New_York date)"
+            printf -- 'source code: https://github.com/zucchini/cios-counter/\n'
+        } >"$sem/status.next.txt"
+
+        # Update the file atomically if we can (that is, if we crash, leave it alone)
+        mv "$sem/status.next.txt" "$sem/status.txt"
+    done
 
     sleep 5m
 done
